c53f1ba5969a0d8463d90175b766968ca39559aa
Tristan Rivoallan <tristan@rivoallan.net>
2024-09-28 10:44:00 +0200

    feat: enable twig

--- public/index.php
+++ public/index.php
@@ -3,14 +3,24 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Salut les Racounes <3");
-    return $response;
+// Setup Twig
+// https://www.slimframework.com/docs/v4/features/twig-view.html
+$twig = Twig::create(__DIR__ . '/../templates', ['cache' => false]);
+$app->add(TwigMiddleware::create($app, $twig));
+
+// Homepage
+$app->get('/', function ($request, $response) {
+    $view = Twig::fromRequest($request);
+    return $view->render($response, 'home.html.twig', [
+        'name' => 'Racounette',
+    ]);
 });
 
 $app->run();
